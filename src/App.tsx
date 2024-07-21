c6f9677d7981690581a5c72dc362a901682b7b05
Vinodh Thangavel <vinodhec@gmail.com>
2024-07-21 19:27:17 +0530

    Refactor Card component styling in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from "react";
 
 function Card() {
   return (
-    <div className="border-black-500 p-2">
-      <div className="border-black-200 p-2 text-2xl font-bold">
-        Card Heading
-      </div>
+    <div className="rounded bg-gray-200 border-gray-400 text-gray-700 text-2xl p-6">
+      <div className="p-4 mb-2 font-bold">Card Heading</div>
 
-      <div className="text-base font-light">Card content</div>
+      <div className="p-4">Card content</div>
     </div>
   );
 }
@@ -15,17 +13,11 @@ function Card() {
 function App() {
   return (
     <div>
-      <header className="bg-red-500 text-white-500 text-2xl p-4 text-center font-bold">
+      <header className="bg-blue-300 text-white-300 text-xl font-bold px-2 text-center">
         Vinodh practice
       </header>
-      <main className="bg-blue-200 text-black text-base p-2 text-left my-2 font-light">
-        I want to be a pro in everything I do
-        <Card></Card>
-      </main>
 
-      <footer className="text-xl p-4 text-center bg-green-400 border-red-800 font-semibold">
-        Footer text
-      </footer>
+      <Card></Card>
     </div>
   );
 }
